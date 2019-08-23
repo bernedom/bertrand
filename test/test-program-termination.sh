a84cf303d5bfc5c69f766cb5a146c51cfe19ce78
Dominik Berner <dominik.berner@gmail.com>
2019-08-23 17:34:09 +0200

    Contracts ignored with NDEBUG

--- test/test-program-termination.sh
+++ test/test-program-termination.sh
@@ -16,23 +16,23 @@ tearDown(){
 }
 
 testForAbnormalProgrammTerminationInDebugMode(){
-    cmake ${ROOT_DIR} -B${BUILD_DIR} -G Ninja > /dev/null
-    cmake --build ${BUILD_DIR} --config Debug --target failing_contract
+    cmake -DCMAKE_BUILD_TYPE=Debug ${ROOT_DIR} -B${BUILD_DIR} -G Ninja > /dev/null
+    cmake --build ${BUILD_DIR} --target failing_contract
     assertEquals "build successful" 0 $?
     assertTrue "Test executable exists" "[ -f "${BUILD_DIR}/test/failing_contract" ]"
     
-    ${BUILD_DIR}/test/failing_contract
+    $(${BUILD_DIR}/test/failing_contract)
     assertNotEquals "Executabled terminated non-zero" $? 0
 }
 
 testForNormalProgrammTerminationInReleaseMode(){
-    cmake ${ROOT_DIR} -B${BUILD_DIR} -G Ninja > /dev/null
-    cmake --build ${BUILD_DIR} --config Release --target failing_contract
+    cmake  -DCMAKE_BUILD_TYPE=Release ${ROOT_DIR} -B${BUILD_DIR} -G Ninja > /dev/null
+    cmake --build ${BUILD_DIR} --target failing_contract
     assertEquals "build successful" 0 $?
     assertTrue "Test executable exists" "[ -f "${BUILD_DIR}/test/failing_contract" ]"
     
-    ${BUILD_DIR}/test/failing_contract
-    assertEquals "Executabled terminated normally" $? 0
+    $(${BUILD_DIR}/test/failing_contract)
+    assertEquals "Executabled terminated normally"  0 $?
 }
 
 
