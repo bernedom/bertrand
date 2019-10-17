46d259f0131e9546e2e87ffa1eba09365c003199
Dominik Berner <dominik.berner@gmail.com>
2019-10-17 22:17:06 +0200

    Added installation test for conan package

--- test/installation-tests.sh
+++ test/installation-tests.sh
@@ -38,7 +38,7 @@ testVersionNumberConsistency()
 }
 
 testPureCmakeInstallation(){
-    # install SI
+    # install bertrand
     cmake ${ROOT_DIR} -B${BERTRAND_BUILD_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -DBUILD_TESTING=off -G Ninja
     cmake --build ${BERTRAND_BUILD_DIR} --config Release --target install
     assertEquals "Installation build successful" 0 $?
@@ -49,16 +49,37 @@ testPureCmakeInstallation(){
 }
 
 testCpackInstallation(){
-    # install SI
-    cmake ${ROOT_DIR} -B${BERTRAND_BUILD_DIR} -DCPACK_PACKAGE_FILE_NAME=install-SI -DBUILD_TESTING=off -G Ninja
+    # install bertrand
+    cmake ${ROOT_DIR} -B${BERTRAND_BUILD_DIR} -DCPACK_PACKAGE_FILE_NAME=install-bertrand -DBUILD_TESTING=off -G Ninja
     cmake --build ${BERTRAND_BUILD_DIR} --config Release --target package
     assertEquals "Installation build successful" 0 $?
-    ${BERTRAND_BUILD_DIR}/install-SI.sh --prefix=${INSTALL_PATH} --skip-license --exclude-subdir
+    ${BERTRAND_BUILD_DIR}/install-bertrand.sh --prefix=${INSTALL_PATH} --skip-license --exclude-subdir
     assertEquals "Installation script successful" 0 $?
     
     cmake ${ROOT_DIR}/test/installation-test -B${BUILD_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_PATH} -G Ninja
     cmake --build ${BUILD_DIR}
     assertEquals "build against installation successful" 0 $?
+    
+}
+
+testConanInstallation()
+{
+    
+    # preliminary cleanup
+    conan remove -f *@bertrand/testing
+    
+    conan create ${ROOT_DIR} bertrand/testing
+    assertEquals "Conan installation build successful" 0 $?
+    conan install -if ${BUILD_DIR} ${ROOT_DIR}/test/conan-installation-test
+    assertEquals "Conan installation successful" 0 $?
+    
+    cmake ${ROOT_DIR}/test/conan-installation-test -B${BUILD_DIR} -G Ninja
+    cmake --build ${BUILD_DIR}
+    assertEquals "build against installation successful" 0 $?
+    
+    # cleanup
+    conan remove -f *@bertrand/testing
+    
     
 }
 
