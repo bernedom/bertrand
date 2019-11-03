fe1b026ae4567701c5daf843d75f5346b330da38
Dominik Berner <dominik.berner@gmail.com>
2019-11-03 22:27:19 +0100

    Testing for message on stderr

--- test/src/text_tests.cc
+++ test/src/text_tests.cc
@@ -1,5 +1,6 @@
 #include <catch.hpp>
-#include <cstdint>
+
+#include <iostream>
 #include <limits>
 #include <sstream>
 
@@ -30,4 +31,23 @@ TEST_CASE(
 TEST_CASE("GIVEN an example function WHEN executed with invalid arguments THEN "
           "contract is triggered") {
   REQUIRE_THROWS(divide(10.0f, 0.0f));
+}
+
+TEST_CASE("GIVEN an example function WHEN executed with invalid arguments THEN "
+          "contract is triggered AND message is set with correct location") {
+  std::stringstream expected;
+  expected << __FILE__ << ":";
+  expected << 11 << ": ('divisor != 0') No division by zero\n";
+  REQUIRE_THROWS_WITH(divide(10.0f, 0.0f), expected.str());
+}
+
+TEST_CASE("GIVEN a contract WHEN it fails THEN message is printed to stderr") {
+  std::stringstream new_stderr;
+  auto old_stderr = std::cerr.rdbuf(new_stderr.rdbuf());
+  std::stringstream expected;
+  expected << __FILE__ << ":";
+  expected << __LINE__ + 1 << ": ('false') Cannot be false\n";
+  REQUIRE_THROWS_WITH(require(false, "Cannot be false"), expected.str());
+  REQUIRE(expected.str().compare(new_stderr.str()) == 0);
+  std::cerr.rdbuf(old_stderr);
 }
