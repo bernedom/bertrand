ea9eda79590dd2f37be81497955d7613be155edf
Dominik Berner <dominik.berner@gmail.com>
2019-12-07 22:27:25 +0100

    Version number bump

--- include/bertrand/bertrand.hpp
+++ include/bertrand/bertrand.hpp
@@ -1,5 +1,5 @@
 /**
- * This file is part of "bertrand" version 0.0.5
+ * This file is part of "bertrand" version 0.0.6
  * https://github.com/bernedom/bertrand
  * A minimalistic, header only implementation of design by contract for C++
  *
