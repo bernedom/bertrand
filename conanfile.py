6d17a5bbbe67d8a655bf57e8ff68053a43b38632
Dominik Berner <dominik.berner@gmail.com>
2020-08-09 22:18:30 +0200

    Version bump to 0.0.11

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class bertrandConan(ConanFile):
     name = "bertrand"
-    version = "0.0.10"
+    version = "0.0.11"
     license = "LGPLv3"
     author = "Dominik Berner <dominik.berner+bertrand-conan@gmail.com"
     url = "https://github.com/bernedom/bertrand"
